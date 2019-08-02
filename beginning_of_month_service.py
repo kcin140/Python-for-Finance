30956b1f076b59e5e9f1f9a337352622fe0770d1
kcin140 <52329381+kcin140@users.noreply.github.com>
2019-08-02 17:09:53 -0500

    Add files via upload

---
+++ beginning_of_month_service.py
@@ -0,0 +1,24 @@
+from pandas_datareader import data
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def beg_of_month_DCA(x, start_date, end_date, monthly_contribution):
+    panel_data = data.DataReader(x, 'yahoo', start_date, end_date, monthly_contribution)['Adj Close']
+    all_end = pd.date_range(start=start_date, end=end_date, freq='BMS')
+    close = panel_data.reindex(all_end)
+    close = close.fillna(method='ffill')
+    most_recent = close[-1]
+    amount_invested = close.count() * monthly_contribution
+    total = 0
+    for y in close:
+        total = total + (monthly_contribution/y)
+    final_value = total * most_recent
+    final_return = ((final_value - amount_invested)/amount_invested) * 100
+    print("Stock: " + str(x))
+    print("Monthly Contribution: $" + str(monthly_contribution))
+    print("Start Date: " + str(start_date))
+    print("End Date: " + str(end_date))
+    print("Total amount invested: $" + str(amount_invested))
+    print("Final investment worth: $" + str(round(final_value, 2)))
+    print("Total Return: " + str(round(final_return, 2)) + "%")
